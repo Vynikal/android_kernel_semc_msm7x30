45950a10f305b308e0701c9f9e80c7ab3d8c8233
Vynikal <Vynikal@seznam.cz>
2014-12-30 21:11:21 +0100

    Merge
    Change-Id: I43e85850d6ce66c4ffd2e3b39231688edfa092e3

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -2,8 +2,8 @@
 PERL		= perl
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-
 # If graphite is enabled, export it so the kernel knows about it, otherwise it could stay to android environment only
+
 ifeq ($(ENABLE_GRAPHITE),true)
 export ENABLE_GRAPHITE := true
 endif
